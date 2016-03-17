269d0eeda3b993447055e966fe3d0cc85c7a22f3
Mohamed Rwash <m.rwash93@yahoo.com>
2016-03-17 23:16:49 +0200

    Initialize repository

---
+++ app/src/main/java/com/rwash/popularmovieapp/GridFragment.java
@@ -0,0 +1,69 @@
+package com.rwash.popularmovieapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class GridFragment extends Fragment {
+
+    private String[] strings = {"dog2", "dog3",
+            "dog4", "dog5",
+            "dog6", "dog7",
+            "dog0", "dog1",
+            "dog2", "dog3",
+            "dog4", "dog5",
+            "dog6", "dog7",
+            "dog0", "dog1",
+            "dog2", "dog3",
+            "dog4", "dog5",
+            "dog6", "dog7"};
+
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+    public GridFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_grid, container, false);
+
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
+        gridview.setAdapter(new CustomAdapter(getActivity(), mThumbIds, strings));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        return rootView;
+    }
+}
